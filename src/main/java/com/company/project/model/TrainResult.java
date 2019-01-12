e908ca7842aa5b442b16062a230c4bbd12cd94d5
Fancy-Block <crstina1113@sina.com>
2019-01-13 03:02:37 +0800

    okok

---
+++ src/main/java/com/company/project/model/TrainResult.java
@@ -0,0 +1,35 @@
+package com.company.project.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.DoubleToIntFunction;
+
+public class TrainResult {
+
+    public SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+    public String trainnumber;
+
+    public int startorder;
+    public int arriveorder;
+
+    public String startstation;
+    public String arrivestation;
+
+    public Date traindate;
+    public String traindateresult;
+
+    public Date arrivetime;
+    public String arrivetimeresult;
+
+    public Date leavetime;
+    public String leavetimeresult;
+
+    public int remainingseat;
+
+    public Date duration;
+    public String durationresult;
+
+    public int price;
+
+}
