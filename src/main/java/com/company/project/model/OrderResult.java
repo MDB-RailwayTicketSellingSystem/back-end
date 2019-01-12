c6e70d5be821c0879e37386f330ccd6fe878f9e4
Fancy-Block <crstina1113@sina.com>
2019-01-13 00:10:42 +0800

    resultType

---
+++ src/main/java/com/company/project/model/OrderResult.java
@@ -0,0 +1,27 @@
+package com.company.project.model;
+
+import java.util.Date;
+
+public class OrderResult {
+
+    public String orderid;
+
+    public String name;
+
+    public Date traindate;
+
+    public String trainnumber;
+
+    public String startstation;
+
+    public String arrivelocation;
+
+    public int price;
+
+    public int state;
+
+    public Date orderdate;
+
+
+
+}
